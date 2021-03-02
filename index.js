d52dd12f19117e1e0518a7a129c3fd34c88d30b6
gadzilla143 <egor1231232@gmail.com>
2021-03-03 00:11:47 +0300

    feat: add user-list

--- index.js
+++ index.js
@@ -1,58 +1,68 @@
 const userList = [
-    {
-        name: 'Dzmitry Antonenka',
-        nativeName: 'Дмитрий Антоненка',
-        department: 'Web & Mobile',
-        avatar: '',
-        room: '1608'
-    },
-    {
-        name: 'Aleh Zhukau',
-        nativeName: 'Олег Жуков',
-        department: 'Web & Mobile',
-        avatar: '',
-        room: '1608'
-    },
-    {
-        name: 'Maxim Podolsky',
-        nativeName: 'Максим Подольский',
-        department: 'Web & Mobile',
-        avatar: '',
-        room: '1608'
-    },
-    {
-        name: 'Anna Belova',
-        nativeName: 'Анна Белова',
-        department: 'Web & Mobile',
-        avatar: '',
-        room: '1608'
-    },
-    {
-        name: 'Vitaliy Vlasov',
-        nativeName: 'Виталий Власов',
-        department: 'Web & Mobile',
-        avatar: '',
-        room: '1608'
-    },
-    {
-        name: 'Stepan Smirnov',
-        nativeName: 'Степан Смирнов',
-        department: 'Web & Mobile',
-        avatar: '',
-        room: '1608'
-    }
-]
+  {
+    name: "Dzmitry Antonenka",
+    nativeName: "Дмитрий Антоненка",
+    department: "Web & Mobile",
+    avatar: "dzmitry.jpg",
+    room: "1608",
+  },
+  {
+    name: "Aleh Zhukau",
+    nativeName: "Олег Жуков",
+    department: "Web & Mobile",
+    avatar: "aleh.jpg",
+    room: "1608",
+  },
+  {
+    name: "Maxim Podolsky",
+    nativeName: "Максим Подольский",
+    department: "Web & Mobile",
+    avatar: "maxim.jpg",
+    room: "1608",
+  },
+  {
+    name: "Anna Belova",
+    nativeName: "Анна Белова",
+    department: "Web & Mobile",
+    avatar: "anna.jpg",
+    room: "1608",
+  },
+  {
+    name: "Vitaliy Vlasov",
+    nativeName: "Виталий Власов",
+    department: "Web & Mobile",
+    avatar: "vitaliy.jpg",
+    room: "1608",
+  },
+  {
+    name: "Stepan Smirnov",
+    nativeName: "Степан Смирнов",
+    department: "Web & Mobile",
+    avatar: "stepan.jpg",
+    room: "1608",
+  },
+];
 
-
-
-
-// data.map(el => {
-//     const row = document.createElement('div')
-//     row.classList.add("container__card");
-//     row.innerHTML = 
-//     `
-//         <div>${el.dada}</div>
-//         <div>${el.nene}</div> 
-//     `
-//     document.getElementById("container").appendChild(row)
-// })
+userList.map((el) => {
+  const row = document.createElement("div");
+  row.classList.add("users__card");
+  row.innerHTML = `
+    <div class="users__personal-info">
+        <img src="./assets/user-list/${el.avatar}" alt="aleh">
+        <h2>${el.name}</h2>
+        <p>${el.nativeName}</p>
+    </div>
+    <div class="users__work-info">
+        <div class="users__department">
+            <img src="./assets/case.svg" alt="aleh">
+            ${el.department}
+        </div>
+    
+        <div class="users__room">
+            <img src="./assets/door.svg" alt="aleh">
+            ${el.room}
+        </div>
+    </div>
+    `;
+  document.getElementById("container").appendChild(row);
+});
