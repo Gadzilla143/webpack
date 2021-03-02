1521ebebf6ea57984bce046cde62d17f243b48fd
gadzilla143 <egor1231232@gmail.com>
2021-03-03 00:32:21 +0300

    feat: add search functional

--- index.js
+++ index.js
@@ -43,26 +43,50 @@ const userList = [
   },
 ];
 
-userList.map((el) => {
-  const row = document.createElement("div");
-  row.classList.add("users__card");
-  row.innerHTML = `
-    <div class="users__personal-info">
-        <img src="./assets/user-list/${el.avatar}" alt="aleh">
-        <h2>${el.name}</h2>
-        <p>${el.nativeName}</p>
-    </div>
-    <div class="users__work-info">
-        <div class="users__department">
-            <img src="./assets/case.svg" alt="aleh">
-            ${el.department}
-        </div>
+const userListBlock = document.getElementById('container');
+const searchBar = document.getElementById('searchBar');
+
+
+const displayUsers = (data) => {
+    data.map((el) => {
+        const row = document.createElement("div");
+        row.classList.add("users__card");
+        row.innerHTML = `
+          <div class="users__personal-info">
+              <img src="./assets/user-list/${el.avatar}" alt="aleh">
+              <h2>${el.name}</h2>
+              <p>${el.nativeName}</p>
+          </div>
+          <div class="users__work-info">
+              <div class="users__department">
+                  <img src="./assets/case.svg" alt="aleh">
+                  ${el.department}
+              </div>
+          
+              <div class="users__room">
+                  <img src="./assets/door.svg" alt="aleh">
+                  ${el.room}
+              </div>
+          </div>
+          `;
+          userListBlock.appendChild(row);
+      });
+      
+}
+
+searchBar.addEventListener('keypress', (e) => {
+
+  userListBlock.innerHTML =''
+  const searchString = e.target.value.toLowerCase();
+  const filteredUsers = userList.filter((user) => {
+      return (
+          user.name.toLowerCase().includes(searchString) ||
+          user.nativeName.toLowerCase().includes(searchString)
+      );
+  });
+  displayUsers(filteredUsers);
+  
     
-        <div class="users__room">
-            <img src="./assets/door.svg" alt="aleh">
-            ${el.room}
-        </div>
-    </div>
-    `;
-  document.getElementById("container").appendChild(row);
 });
+
+displayUsers(userList)
