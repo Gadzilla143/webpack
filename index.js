f2e10c79445cc7314b8afb965f233df0f95a09cb
gadzilla143 <egor1231232@gmail.com>
2021-03-02 15:14:06 +0300

    feat: add header

---
+++ index.js
@@ -0,0 +1,44 @@
+// const data = [
+//     {
+//         dada: '222',
+//         nene: '222'
+//     },
+//     {
+//         dada: '333',
+//         nene: '333'
+//     },
+//     {
+//         dada: '444',
+//         nene: '444'
+//     },
+//     {
+//         dada: '222',
+//         nene: '222'
+//     },
+//     {
+//         dada: '333',
+//         nene: '333'
+//     },
+//     {
+//         dada: '444',
+//         nene: '444'
+//     }
+// ]
+
+
+
+
+// data.map(el => {
+//     const row = document.createElement('div')
+//     row.classList.add("container__card");
+//     row.innerHTML = 
+//     `
+//         <div>${el.dada}</div>
+//         <div>${el.nene}</div> 
+//     `
+//     document.getElementById("container").appendChild(row)
+// })
+
+
+
+
